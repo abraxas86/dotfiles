68ab4221fddce85f5e3a2a1416b2f709a4063157
abraxas86 <wardrich@gmail.com>
2025-04-02 22:33:56 -0400

    Not sure about bashrc but nvim was updated for vimwiki. Tmux config was slight UI change

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,20 +1,27 @@
--- Initialize Plugin Manager
+-- Initialize Plugin Managers (Packer & Vim-Plug)
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
 end
 
+local plug_path = vim.fn.stdpath('data')..'/site/autoload/plug.vim'
+if vim.fn.empty(vim.fn.glob(plug_path)) > 0 then
+  vim.fn.system({'sh', '-c', 'curl -fLo ' .. plug_path .. ' --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'})
+end
+
+-- Ensure Packer is loaded
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
 end
 
+-- Initialize Packer and define plugins
 packer.startup(function(use)
   use 'wbthomason/packer.nvim' -- Packer can manage itself
-  use 'preservim/nerdtree'      -- NERDTree plugin
+  use 'preservim/nerdtree'      -- NERDTree plugin for file navigation
   
-  -- Theme
+  -- Theme configuration
   use {
     'scottmckendry/cyberdream.nvim',
     config = function()
@@ -24,6 +31,16 @@ packer.startup(function(use)
       })
     end
   }
+
+-- Initialize Vimplug Plugins:
+vim.cmd [[
+  call plug#begin(stdpath('data') . '/plugged')
+
+  Plug 'vimwiki/vimwiki'     " Vimwiki
+
+  call plug#end()
+]]
+
 
   -- Indentation Guides
   use {
@@ -35,8 +52,8 @@ packer.startup(function(use)
         show_current_context = true,
         show_current_context_start = true,
       }
-
-      -- Make indent lines more visible
+      
+      -- Customize indent line colors
       vim.cmd [[
         highlight IndentBlanklineChar guifg=#FF5555 gui=nocombine
         highlight IndentBlanklineSpaceChar guifg=#FF5555 gui=nocombine
@@ -44,16 +61,15 @@ packer.startup(function(use)
       ]]
     end
   }
-
 end)
 
--- Set colorscheme
+-- Set colorscheme with error handling
 local theme_ok, _ = pcall(vim.cmd, "colorscheme cyberdream")
 if not theme_ok then
   print("Failed to load theme: cyberdream")
 end
 
--- Apply highlights after everything loads
+-- Apply custom highlights after Neovim has fully loaded
 vim.api.nvim_create_autocmd("VimEnter", {
   pattern = "*",
   callback = function()
@@ -67,7 +83,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
--- Force trigger ColorScheme event in case the theme is reloaded later
+-- Ensure ColorScheme autocommand triggers when theme is reloaded
 vim.cmd("doautocmd ColorScheme")
 
 -- Keybinding to toggle between light and dark mode
@@ -82,10 +98,10 @@ vim.api.nvim_create_autocmd("User", {
   end,
 })
 
--- Show line numbers
+-- Enable line numbers
 vim.o.number = true
 
--- Open NERDTree automatically but do not take focus
+-- Open NERDTree automatically on startup, but do not take focus
 vim.api.nvim_create_autocmd("VimEnter", {
   pattern = "*",
   callback = function()
@@ -96,26 +112,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
----------- CUSTOM COMMANDS ---------- 
+---------- CUSTOM COMMANDS ----------
 
--- Custom Clip command (Copy all contents to clipboard)
+-- Custom command: Copy entire file to clipboard
 vim.api.nvim_create_user_command('Clip', function()
   vim.cmd("normal! ggVG\"+y")
 end, {})
 
--- Reload VIM config
+-- Custom command: Reload Neovim configuration
 vim.api.nvim_create_user_command('ReloadConfig', function()
     vim.cmd("source $MYVIMRC")
 end, {})
 
--- Tab fixes
+---------- INDENTATION SETTINGS ----------
+
+-- Set tab behavior
 vim.opt.tabstop = 4      -- Number of spaces that <tab> counts for
 vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of auto-indent
 vim.opt.expandtab = true -- Convert tabs to spaces
 
------- KEYMAPS -----
+---------- KEYMAPS ----------
 
--- Remap arrows to visual lines instead of logical lines
+-- Remap arrow keys to move by visual lines instead of logical lines
 vim.api.nvim_set_keymap('n', '<Down>', 'gj', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Up>', 'gk', { noremap = true })
-
